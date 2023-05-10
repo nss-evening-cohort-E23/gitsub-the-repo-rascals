3204e6783a40b0c75030eb9effd5764461b0aee4
Eli Bradford <elibradford227@gmail.com>
2023-05-09 20:33:48 -0500

    modified repo object and repo.js

--- data/repos.js
+++ data/repos.js
@@ -3,10 +3,12 @@ export const repos = [
     id: 1,
     name: 'Calculator',
     desc: 'A fully functional vanilla JS calculator',
+    isPinned: false
   },
   {
     id: 2,
     name: 'Original LOZ Clone',
     desc: 'A clone of the original Legend of Zelda built using Python',
+    isPinned: false
   },
 ]
